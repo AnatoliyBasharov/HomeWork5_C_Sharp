abbce013a71c713d85d3e7919db9338df367b700
Anatoliy Basharov <rexonka3@gmail.com>
2023-06-26 18:14:59 +0300

    Finish HomeTask2

--- HomeTask2/Program.cs
+++ HomeTask2/Program.cs
@@ -3,3 +3,34 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+int[] Generate(int Len, int min, int max)
+{
+    int[] arrayrnd = new int[Len];
+    Random rnd = new Random();
+    for (int i = 0; i < arrayrnd.Length; i++)
+    {
+        arrayrnd[i] = rnd.Next(min, max + 1);
+    }
+    return arrayrnd;
+}
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write("\t" + item);
+    }
+}
+int[] array = Generate(6, -20, 20);
+PrintArray(array);
+int SumArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        i++;                                          //Если считать как программисты то i++ выше суммы,                                                        
+        sum += array[i];                                    //если как обычные люди то i++ ниже суммы
+        
+    }
+    return sum;
+}
+System.Console.WriteLine($" --> {SumArray(array)}");
